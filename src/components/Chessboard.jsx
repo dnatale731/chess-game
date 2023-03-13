c273fc403394c9f257ae3856a919cf8fe93eb7f5
Domenico Natale <domenico.natale@pixida.com>
2023-03-13 16:40:24 -0400

    Improved initial board state

--- src/components/Chessboard.jsx
+++ src/components/Chessboard.jsx
@@ -9,22 +9,20 @@ const pieces = [];
 for (let i = 0; i < 8; i++) {
   pieces.push({ image: "assets/pawn_d.png", x: i, y: 6 });
   pieces.push({ image: "assets/pawn_l.png", x: i, y: 1 });
-  if (i === 0 || i === 7) {
-    pieces.push({ image: "assets/rook_d.png", x: i, y: 7 });
-    pieces.push({ image: "assets/rook_l.png", x: i, y: 0 });
-  }
-  if (i === 1 || i === 6) {
-    pieces.push({ image: "assets/knight_d.png", x: i, y: 7 });
-    pieces.push({ image: "assets/knight_l.png", x: i, y: 0 });
-  }
-  if (i === 2 || i === 5) {
-    pieces.push({ image: "assets/bishop_d.png", x: i, y: 7 });
-    pieces.push({ image: "assets/bishop_l.png", x: i, y: 0 });
-  }
-  pieces.push({ image: "assets/queen_d.png", x: 3, y: 7 });
-  pieces.push({ image: "assets/queen_l.png", x: 3, y: 0 });
-  pieces.push({ image: "assets/king_d.png", x: 4, y: 7 });
-  pieces.push({ image: "assets/king_l.png", x: 4, y: 0 });
+}
+
+for (let p = 0; p < 2; p++) {
+  const type = p === 0 ? "d" : "l";
+  const y = p === 0 ? 7 : 0;
+
+  pieces.push({ image: `assets/rook_${type}.png`, x: 0, y: y });
+  pieces.push({ image: `assets/rook_${type}.png`, x: 7, y: y });
+  pieces.push({ image: `assets/knight_${type}.png`, x: 1, y: y });
+  pieces.push({ image: `assets/knight_${type}.png`, x: 6, y: y });
+  pieces.push({ image: `assets/bishop_${type}.png`, x: 2, y: y });
+  pieces.push({ image: `assets/bishop_${type}.png`, x: 5, y: y });
+  pieces.push({ image: `assets/queen_${type}.png`, x: 3, y: y });
+  pieces.push({ image: `assets/king_${type}.png`, x: 4, y: y });
 }
 
 export default function Chessboard() {
